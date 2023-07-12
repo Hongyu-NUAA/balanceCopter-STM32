c2263656471b902d84291c8b1e8791120b60f2d2
Hongyu-996 <126148015+Hongyu-NUAA@users.noreply.github.com>
2023-07-12 22:32:03 +0800

    控制输出完成

--- Core/Inc/stm32f4xx_it.h
+++ Core/Inc/stm32f4xx_it.h
@@ -55,6 +55,7 @@ void DebugMon_Handler(void);
 void SysTick_Handler(void);
 void CAN1_RX0_IRQHandler(void);
 void USART1_IRQHandler(void);
+void DMA2_Stream2_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
--- boards/bsp_uart.h
+++ boards/bsp_uart.h
@@ -3,6 +3,7 @@
 
 #include "usart.h"
 
+#define PACKED __packed
 #define MOTOR_MAX_SPEED 1000 // 此处为最大的电机转速
 
 #define RM_UART_MAX_LEN    50
@@ -11,38 +12,42 @@
 extern uint8_t UART1_RX_BUF[RM_UART_MAX_LEN]; // 缓存数组
 extern uint8_t UART1_RX_LEN;                  // 缓存数组长度
 
-struct __packed rmuart_struct {
+
+
+struct PACKED apm_2_stm32_struct {
     uint8_t header[2];
     uint8_t len;
-    int16_t wheel_left;
-    int16_t wheel_right;
+    int16_t wheel_left_int;
+    int16_t wheel_right_int;
 };
-union rmuart_t {
-    struct rmuart_struct rmuart_s;
-    uint8_t bits[sizeof(struct rmuart_struct)];
+union apm_2_stm32_union {
+    struct apm_2_stm32_struct apm_2_stm32_t;
+    uint8_t bits[sizeof(struct apm_2_stm32_struct)];
 };
 
-struct __packed ardupilot_struct {
+struct PACKED stm32_2_apm_struct {
     uint8_t header[2];
     uint8_t len;
-    int16_t wheel_left_speed;
-    int16_t wheel_right_speed;
+    int16_t wheel_left_int;
+    int16_t wheel_right_int;
 };
-union ardupilot_t {
-    struct ardupilot_struct ardupilot_s;
+union stm32_2_apm_union {
+    struct stm32_2_apm_struct stm32_2_apm_t;
 
-    uint8_t bits[sizeof(struct ardupilot_struct)];
+    uint8_t bits[sizeof(struct stm32_2_apm_struct)];
 };
+
 
 void bsp_uart_parse(uint8_t data);
 
-void bsp_uart1_set_wheel_speed(int16_t wheelleft, int16_t wheelright);
+void bsp_uart1_get_feedback_wheel_speed(int16_t wheelleft, int16_t wheelright);
 
-void bsp_uart_get_wheel_speed(int16_t* wheelleft, int16_t* wheelright);
+void bsp_uart_get_target_wheel_speed(int16_t* wheelleft, int16_t* wheelright);
 
 void bsp_uart1_init(void);
 
 void bsp_uart1_tx(void);
 
+void bsp_uart1_handler(void);
 
 #endif
